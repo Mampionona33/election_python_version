1244bb8e3441ce4933b694fe5eb76a4dc2d700d9
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-08 09:49:04 +0000

    Update 2 files

--- app/poll/views.py
+++ app/poll/views.py
@@ -1,19 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.shortcuts import render
+from django.http import Http404
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("poll/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("poll/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "poll/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "poll/detail.html", {"question": question})
 
 
 def results(request, question_id):
